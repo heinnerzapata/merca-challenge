02b0bb47ec7d73656cd91e76e380ad9c7a44982c
Heinner Zapata Escarraga <heinner.zapata@globant.com>
2017-09-07 23:10:26 -0500

    load coordinates structure

--- src/js/controllers/HomeCtrl.js
+++ src/js/controllers/HomeCtrl.js
@@ -2,6 +2,27 @@ angular.module('helloWorldApp')
 .controller('HomeCtrl' , [
   '$scope',
   function($scope){
-    $scope.message = 'HHHHHHHHHHHHHHHHHHHHHHHHHHH';
+
+    $scope.message = 'heinner zapata';
+
+    $scope.map;
+
+    $scope.initMap = function() {
+
+      var map_div = angular.element(document.querySelector('#map_div'));
+
+      if(map_div.length){
+        $scope.map = new google.maps.Map(document.getElementById('map_div'), {
+           center: {lat: -34.397, lng: 150.644},
+           zoom: 8
+        });
+      }
+
+    }
+
+    $scope.initMap();
+
+    google.maps.event.addDomListener(window, 'load', $scope.initMap);
+
   }
 ]);
