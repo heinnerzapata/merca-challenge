1139dae45d32697b651bfa199df4e3710b0e0fe8
Heinner Zapata Escarraga <heinner.zapata@globant.com>
2017-09-13 01:59:37 -0500

    azure api

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -35476,8 +35476,10 @@ angular.module('helloWorldApp')
   return {
     api : {
       geolocationByAddresss : 'http://localhost:3002/api/geolocationByAddresss',
-      uploadFiles : 'http://localhost:3002/api/uploadFiles',
-      getDistanceTwoPoints : 'http://localhost:3002/api/getDistanceTwoPoints'
+      //uploadFiles : 'http://localhost:3002/api/uploadFiles',
+      //getDistanceTwoPoints : 'http://localhost:3002/api/getDistanceTwoPoints'
+      uploadFiles : 'https://merca-challenge-api2.azurewebsites.net/api/uploadFiles',
+      getDistanceTwoPoints : 'https://merca-challenge-api2.azurewebsites.net/api/getDistanceTwoPoints'
     }
   }
 });
